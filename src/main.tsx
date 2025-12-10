e2ac7c438e81d32a27e981495ec7fea58169b39b
Anastasia-Artemova <anastasia.r.artemova@gmail.com>
2025-12-10 11:37:03 +0100

    Updated routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Basket from './Basket.tsx'
 import { ThemeProvider } from '@mui/material'
 import theme from './theme.ts'
@@ -14,6 +14,8 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/products" replace />} />
+
           <Route path="/products" element={<App />} />
           <Route path="/basket" element = {<Basket/>}/>
           <Route path="/products/:id" element={<ProductDetailWrapper />} /> 
